9e71e55b5069158297452f5c3e5ed5cbb784d53c
Richard L. Sypert Jr <Rsypertjr@users.noreply.github.com>
2017-05-31 16:58:44 -0700

    removed jsonserver from server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
-
+/*
 var jsonServer = require('json-server');
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
@@ -18,4 +18,5 @@ server.use(router);
 server.listen(port, function () {
   console.log('JSON Server is running')
 });
+*/
 
