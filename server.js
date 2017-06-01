e29cb36edd8d305ad9eee4c3b32f2dfc2b6078ee
Richard L. Sypert Jr <Rsypertjr@users.noreply.github.com>
2017-05-31 18:10:35 -0700

    moved jsonServer to service file

--- server.js
+++ server.js
@@ -8,16 +8,7 @@ app.use(express.static(__dirname + '/dist'));
 // Heroku port
 app.listen(process.env.PORT || 8080);
 /*
-var jsonServer = require('json-server');
-var server = jsonServer.create();
-var router = jsonServer.router('db.json');
-var middlewares = jsonServer.defaults();
-var port = Number(process.env.PORT || 3000);
-server.use(middlewares);
-server.use(router);
-server.listen(port, function () {
-  console.log('JSON Server is running')
-});
+
 */
 
 
--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -9,6 +9,17 @@ const BASE_URL2 = 'http://localhost:3000/db';
 const BASE_URL = 'http://localhost:3000/users';
 const HEADER = { headers: new Headers({ 'Content-Type': 'application/json' }) };
 
+var jsonServer = require('json-server');
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+var port = Number(process.env.PORT || 3000);
+server.use(middlewares);
+server.use(router);
+server.listen(port, function () {
+  console.log('JSON Server is running')
+});
+
 @Injectable()
 export class UserService {
     user:URLSearchParams;
