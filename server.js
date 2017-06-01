4d22ca2619e2fd393fe180281618b6728ae1e566
Richard L. Sypert Jr <Rsypertjr@users.noreply.github.com>
2017-05-31 18:16:27 -0700

    redeploy

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
-/*
 
-*/
+var jsonServer = require('json-server');
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+var port = 3000;
+server.use(middlewares);
+server.use(router);
+server.listen(port, function () {
+  console.log('JSON Server is running')
+});
+
 
 
